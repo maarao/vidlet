db8ac3fd48fd646449b4505cf03de418704880a7
samalash <samalash@udel.edu>
2024-03-16 18:08:33 -0400

    Feat: Implemented generateFlashcards function

--- app/scripts/generateFlashcards.py
+++ app/scripts/generateFlashcards.py
@@ -5,15 +5,26 @@ from dotenv import load_dotenv
 load_dotenv()
 client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
 
+def getTranscriptFromFile(filename):
+    try:
+        with open(os.path.join(os.path.dirname(__file__), filename), "r") as file:
+            text = file.read()
+            return text
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+        return None
+    
+def generateFlashcards(videoDurationInMinutes):
+    transcript = getTranscriptFromFile("transcript/transcript.txt")
 
-response = client.chat.completions.create(
-    max_tokens=30,
-    model="gpt-3.5-turbo",
-    response_format={ "type": "json_object" },
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
-        {"role": "user", "content": "Say this is a test!"}
-    ]
-)
+    response = client.chat.completions.create(
+        max_tokens=4096,
+        model="gpt-3.5-turbo-0125",
+        response_format={ "type": "json_object" },
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+            {"role": "user", "content": "Use this educational video transcript to generate " + videoDurationInMinutes + " flashcards, each flashcard having a term and definition, based on the information described in the video: " + transcript},
+        ]
+    )
 
-print(response.choices[0].message.content)
+    print(response.choices[0].message.content)
