5ce271fc735aa0690891995d9fdc703acfabd59e
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-03-16 20:25:18 -0400

    Feat: Created questions component

---
+++ app/quiz/page.tsx
@@ -0,0 +1,13 @@
+import Question from '/app/components/quiz/question';
+import test from 'app/scripts/flashcards/flashcards.json' assert { type: 'json' };
+
+export default function QuizPage() {
+  return (
+    <div>
+      <h1>Quiz Page</h1>
+      <div className="flex justify-center">
+      <Question ></Question>
+      </div>
+    </div>
+  );
+}
