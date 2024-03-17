125b9962ed5ffaaf5a71d3ff84140064f981ba07
samalash <samalash@udel.edu>
2024-03-17 10:40:18 -0400

    Feat: Start formatting layout and landing page

--- app/components/upload.tsx
+++ app/components/upload.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react';
+import { Label, FileInput } from 'flowbite-react';
 
 export default function Upload() {
   const [file, setFile] = useState(null);
@@ -30,8 +31,34 @@ export default function Upload() {
   };
 
   return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
+    <div style={{ width:'400px', height:'300px'}}>
+      <Label
+        htmlFor="dropzone-file"
+        className="dark:hover:bg-bray-800 flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+      >
+        <div className="flex flex-col items-center justify-center pb-6 pt-5">
+          <svg
+            className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 16"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+            />
+          </svg>
+          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+            <span className="font-semibold">Click to upload</span> or drag and drop
+          </p>
+          <p className="text-xs text-gray-500 dark:text-gray-400">Video or SRT file</p>
+        </div>
+        <FileInput id="dropzone-file" accept="video/*,.srt" className="hidden" onChange={handleFileChange} />
+      </Label>
       <button onClick={handleUpload}>Upload</button>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import Upload from "/app/components/upload";
-import Generate from "/app/components/generate";
+import Upload from "./components/upload";
+import Generate from "./components/generate";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className="text-3xl w-64 pb-10">With Vidlet, anyone can easily upload an educational video and receive back flashcards to study with—in under a minute.</p>
       <Upload></Upload>
       <Generate></Generate>
     </main>
