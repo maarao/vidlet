b55cb768b6829f6b01a74558a80aa20322287447
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-03-16 22:12:26 -0400

    Feat: Create a button component to run the python script

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Upload from "/app/components/upload";
+import Generate from "/app/components/generate";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Upload></Upload>
+      <Generate></Generate>
     </main>
   );
 }
---
+++ pages/api/runPythonScript.ts
@@ -0,0 +1,19 @@
+import { spawn } from 'child_process';
+
+export default async function handler(req, res) {
+  const pythonProcess = spawn('python', ['./app/scripts/convert.py']);
+
+  pythonProcess.stdout.on('data', (data) => {
+    console.log(`Python Script Output: ${data.toString()}`);
+  });
+
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(`Python Script Error: ${data.toString()}`);
+    res.status(500).json({ error: 'Error running Python script' });
+  });
+
+  pythonProcess.on('close', (code) => {
+    console.log(`Python Script exited with code: ${code}`);
+    res.status(200).json({ message: 'Python script executed successfully' });
+  });
+}
