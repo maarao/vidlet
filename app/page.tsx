f56e7255085a1bb1a95ac647cc4795dd007a27d4
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-03-17 11:10:37 -0400

    made landing page non-scrollable

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Generate from "./components/generate";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex max-h-screen flex-col items-center justify-between p-24">
       <p className="text-3xl w-64 pb-10">With Vidlet, anyone can easily upload an educational video and receive back flashcards to study with—in under a minute.</p>
       <Upload></Upload>
       <Generate></Generate>
