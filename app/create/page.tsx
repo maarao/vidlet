3d4a8ab7519e3c9ec3818685f2e70b91920d9f36
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-03-22 15:35:32 -0400

    Feat: Implemented update to state variable name

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -18,11 +18,21 @@ const Create = () => {
     const [uid, setUid] = useState<number>(Date.now());
 
     const [flashcards, setFlashcards] = useState<FlashcardSet>({key: uid, name: "Unnamed Set", cards: []});
+
+    const [name, setName] = useState<string>("");
+
     const [term, setTerm] = useState<string>("");
     const [definition, setDefinition] = useState<string>("");
 
     return (
         <div>
+            <h1>Add Term and Definition</h1>
+            <div>
+                <label>
+                    Name:
+                    <input type="text" value={name} onChange={(e) => {setName(e.target.value)}} />
+                </label>
+            </div>
             
         </div>
     )
