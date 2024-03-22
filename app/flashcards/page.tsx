43f9fe20fcfa22bc56781610610a9526b5a4403c
samalash <samalash@udel.edu>
2024-03-22 11:53:59 -0400

    Feat: Inserted Select Flashcards input into page

--- app/flashcards/page.tsx
+++ app/flashcards/page.tsx
@@ -1,3 +1,5 @@
+import Flashcard from "./components/flashcard"
+import { Card, CardContent } from "@/components/ui/card"
 import {
     Carousel,
     CarouselContent,
@@ -5,16 +7,38 @@ import {
     CarouselNext,
     CarouselPrevious,
   } from "@/components/ui/carousel"
-  import { Card, CardContent } from "@/components/ui/card"
-  import Flashcard from "./components/flashcard"
+  import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
 
 
 
 export default function Home() {
     return (
         <div>
-            <div className="flex items-center justify-center h-screen">
-                <Carousel className="w-full max-w-xs relative top-16">
+            <div className="flex items-center justify-center h-screen relative">
+                <Select>
+                    <SelectTrigger className="w-[180px] absolute z-10 top-32">
+                        <SelectValue placeholder="Select a flashcard set" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                        <SelectLabel>Flashcard Sets</SelectLabel>
+                        <SelectItem value="apple">Apple</SelectItem>
+                        <SelectItem value="banana">Banana</SelectItem>
+                        <SelectItem value="blueberry">Blueberry</SelectItem>
+                        <SelectItem value="grapes">Grapes</SelectItem>
+                        <SelectItem value="pineapple">Pineapple</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+                <Carousel className="w-full max-w-xs relative top-16 z-0">
                     <CarouselContent>
                         {Array.from({ length: 5 }).map((_, index) => (
                         <CarouselItem key={index}>
