f44d932dac1dd2ed15a22a6fea25604ad6f00733
samalash <samalash@udel.edu>
2024-03-21 21:05:06 -0400

    Feat: Create basic carousel

---
+++ app/flashcards/page.tsx
@@ -0,0 +1,36 @@
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
+  import { Card, CardContent } from "@/components/ui/card"
+
+
+
+export default function Home() {
+    return (
+        <div>
+            <div className="flex items-center justify-center h-screen">
+                <Carousel className="w-full max-w-xs relative top-16">
+                    <CarouselContent>
+                        {Array.from({ length: 5 }).map((_, index) => (
+                        <CarouselItem key={index}>
+                            <div className="p-1">
+                            <Card>
+                                <CardContent className="flex aspect-square items-center justify-center p-6">
+                                <span className="text-4xl font-semibold">{index + 1}</span>
+                                </CardContent>
+                            </Card>
+                            </div>
+                        </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
+            </div>
+        </div>
+    )
+}
