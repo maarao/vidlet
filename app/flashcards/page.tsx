72e5b46d63a37885bd51c49649118a752a9add81
samalash <sammyjoseph2003@outlook.com>
2024-03-24 18:24:06 -0400

    Fix: Fixed CarouselContent not stretching height to entire Carousel element

--- app/flashcards/page.tsx
+++ app/flashcards/page.tsx
@@ -8,7 +8,7 @@ import {
     CarouselItem,
     CarouselNext,
     CarouselPrevious,
-  } from "@/components/ui/carousel"
+  } from "./components/flashcard-carousel"
   import {
     Select,
     SelectContent,
@@ -21,45 +21,42 @@ import {
 
 
 
-export default function Home() {
+export default function Flashcards() {
+
     return (
-        <div className="h-full w-full">
-            <div className="flex items-center justify-center border-8 border-red-50 h-full w-full">
-                <Select>
-                    <SelectTrigger className="w-[180px] absolute z-10 top-32">
-                        <SelectValue placeholder="Select a flashcard set" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectGroup>
+        <div className="flex items-center justify-center border-8 border-red-50 h-full w-full">
+            <Select>
+                <SelectTrigger className="w-[15%] absolute z-10 top-32">
+                    <SelectValue placeholder="Select a flashcard set..." />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectGroup>
                         <SelectLabel>Flashcard Sets</SelectLabel>
                         <SelectItem value="apple">Apple</SelectItem>
                         <SelectItem value="banana">Banana</SelectItem>
                         <SelectItem value="blueberry">Blueberry</SelectItem>
                         <SelectItem value="grapes">Grapes</SelectItem>
                         <SelectItem value="pineapple">Pineapple</SelectItem>
-                        </SelectGroup>
-                    </SelectContent>
-                </Select>
-                <Carousel className="absolute top-64 z-0 border-8 w-3/5 h-2/5">
-                    <div className="h-full">
-                        <CarouselContent className="h-full border-8 border-black">
-                            {Array.from({ length: 5 }).map((_, index) => (
-                            <CarouselItem className="h-full" key={index}>
-                                <div className="p-1 h-full">
-                                    <Card className="h-full">
-                                        <CardContent className="flex items-center justify-center p-0 h-full">
-                                            <Flashcard term="hi" definition="yo"></Flashcard>
-                                        </CardContent>
-                                    </Card>
-                                </div>
-                            </CarouselItem>
-                            ))}
-                        </CarouselContent>
-                    </div>
-                    <CarouselPrevious />
-                    <CarouselNext />
-                </Carousel>
-            </div>
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
+            <Carousel className="absolute top-64 z-0 w-3/5 h-2/5">
+                <CarouselContent className="h-full">
+                    {Array.from({ length: 5 }).map((_, index) => (
+                    <CarouselItem key={index}>
+                        <div className="p-1 h-full">
+                            <Card className="h-full">
+                                <CardContent className="flex items-center justify-center align-middle p-0 min-h-full h-full">
+                                    <Flashcard term="hi" definition="yo"></Flashcard>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
         </div>
     )
 }
