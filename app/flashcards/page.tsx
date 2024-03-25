81182604af6106dc882648427226efe20bc64170
samalash <sammyjoseph2003@outlook.com>
2024-03-24 21:28:35 -0400

    Feat: Carousel shows flashcards of selected study set

--- app/flashcards/page.tsx
+++ app/flashcards/page.tsx
@@ -18,16 +18,28 @@ import {
     SelectTrigger,
     SelectValue,
   } from "@/components/ui/select"
+  import { useState } from 'react';
 
 
 
 export default function Flashcards() {
 
-    const flashcardSetKeys:string[] = Object.keys(localStorage);
+    const flashcardSetKeys:string[] = [];
+
+    const [selectedSet, setSelectedSet] = useState<string>("");
+
+    const handleSelect = (value: string) => {
+
+        setSelectedSet(value);
+    }
+
+    Object.keys(localStorage).forEach(function(key){
+        if (!isNaN(+key)) flashcardSetKeys.push(key);
+     });
 
     return (
         <div className="flex items-center justify-center border-8 border-red-50 h-full w-full">
-            <Select>
+            <Select onValueChange={handleSelect}>
                 <SelectTrigger className="w-[15%] absolute z-10 top-32">
                     <SelectValue placeholder="Select a flashcard set..." />
                 </SelectTrigger>
@@ -40,23 +52,26 @@ export default function Flashcards() {
                     </SelectGroup>
                 </SelectContent>
             </Select>
-            <Carousel className="absolute top-64 z-0 w-1/2 h-1/2">
+            {selectedSet != "" ?
+            <Carousel className="absolute top-64 z-0 w-1/2 h-1/2" opts={{startIndex: 0}}>
                 <CarouselContent className="h-full">
-                    {Array.from({ length: 5 }).map((_, index) => (
-                    <CarouselItem key={index}>
-                        <div className="p-1 h-full">
-                            <Card className="h-full cursor-pointer">
-                                <CardContent className="flex items-center justify-center align-middle p-0 min-h-full h-full">
-                                    <Flashcard term="hi" definition="yo"></Flashcard>
-                                </CardContent>
-                            </Card>
-                        </div>
-                    </CarouselItem>
+                    {JSON.parse(localStorage.getItem(selectedSet)!).cards.map((card:{term:string, definition:string}) => (
+                        <CarouselItem>
+                            <div className="p-1 h-full">
+                                <Card className="h-full cursor-pointer">
+                                    <CardContent className="flex items-center justify-center align-middle p-0 min-h-full h-full">
+                                        <Flashcard term={card.term} definition={card.definition}></Flashcard>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </CarouselItem>
                     ))}
                 </CarouselContent>
                 <CarouselPrevious />
                 <CarouselNext />
-            </Carousel>
+            </Carousel> :
+            <p></p>
+            }
         </div>
     )
 }
