c54e2d9a804ba0d6bf8a8a5678d26775aeebb1bb
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-03-26 02:21:45 -0400

    refactor: removed unused import

--- app/create/components/Flashcard.tsx
+++ app/create/components/Flashcard.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useMemo } from "react";
+import { useState } from "react";
 import { Input } from "@/components/ui/input";
 
 const Flashcard = (props: { flashcards, index }) => {
