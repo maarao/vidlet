ae0dfef00488e529b1272e1cd5e758899b8ffb9c
Maanav Rao <58347499+maarao@users.noreply.github.com>
2024-03-26 02:46:50 -0400

    feat: changed reset to just refresh after a 1 second delay to prepare for redirecting the user to the flashcard page

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from "react";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Flashcard } from "./components/flashcard";
 
@@ -36,7 +36,9 @@ const Create = () => {
     }
 
     const cleanUp = () => {
-        setFlashcards({key: Date.now(), version: flashcards.version, name: "Unnamed Set", cards: []});
+        setTimeout(() => {
+            window.location.reload();
+        }, 1000);
     }
 
     const createFlashcardSet = () => {
