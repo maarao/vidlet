76d72ec76bdfaa59ec403d5b4ba92845cf6edec4
samalash <sammyjoseph2003@outlook.com>
2024-03-27 00:19:29 -0400

    Feat: add some padding to separate links

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -15,7 +15,10 @@ import {
     return (
         <NavigationMenu>
             <NavigationMenuList>
-                <NavigationMenuItem>
+                 <NavigationMenuItem className="pr-6">
+                    <a href="/">Home</a>
+                </NavigationMenuItem>
+                <NavigationMenuItem className="pr-6">
                     <a href="/create">Create</a>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
